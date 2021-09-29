009d5edd834792ad3f0ad9077bd9d3fc171355c2
wouldRewind <nik.152002@mail.ru>
2021-09-29 18:23:34 +0300

    now we can choose the category and show/hide sort panel

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,48 +2,22 @@ import React,{useState} from 'react'
 
 
 
-// class Categories extends React.Component {
-
-//   state = {
-//     activeItem: null
-//   };
-
-//   onSelectItem = index => {
-//     this.setState({
-//       activeItem: index
-//     })
-//   }
-
-//   render(){
-//     const {items,onClickItem} = this.props;
-
-//     return(
-//       <div className="categories">
-//                    <ul>
-//                      <li className="active">Все</li>
-//                      {items.map((name,index) => (
-//                          <li className={this.state.activeItem === index ? "active" : null } onClick={() => 
-//                           this.onSelectItem(index)} key={`${name}_${index}`}>{name}</li>
-//                      ))}
-//                    </ul>
-//                  </div>
-//     )
-
-//   }
-// }
 
 function Categories({items,onClickItem}) {
 
     const [activeItem, setActiveItem] = useState(null)
-    console.log(items);
+
+    const onSelectItem = index => setActiveItem(index)
+
     return (
         <div className="categories">
               <ul>
-                <li className={activeItem == null ? "active": ''}>Все</li>
-                {items.map((name,index) => (
+                <li onClick={() => onSelectItem(null)} className={activeItem === null ? "active": ''}>Все</li>
+                {items &&
+                items.map((name,index) => (
                     <li
                     className={activeItem === index ? 'active' : ''} 
-                    onClick={() => setActiveItem(index)} key={`${name}_${index}`}>{name}</li>
+                    onClick={() => onSelectItem(index)} key={`${name}_${index}`}>{name}</li>
                 ))}
               </ul>
             </div>
